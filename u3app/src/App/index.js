69c5e894874c42617f315e7f2849d6534054d28d
mikehoods <62457440+mikehoods@users.noreply.github.com>
2020-06-15 12:12:48 -0500

    working on reverseInt functionality

--- u3app/src/App/index.js
+++ u3app/src/App/index.js
@@ -1,10 +1,39 @@
-import React, { Component } from "react";
+import React, { Component, useImperativeHandle, useEffect } from "react";
 import "./style.css";
 
-class App extends Component {
-  render() {
-    return <div className="App">Hello World</div>;
+
+
+const App = (props) => {
+  const [intReversed, setIntReversed] = React.useState(0)
+
+  
+  const reverseInt = (int) => {
+    if(Math.sign(int) === 1) {
+        intReversed = parseInt(int.toString().split('').reverse().join(""))
+    return intReversed
+    } else if (Math.sign(int) === 0) {
+        intReversed = 0
+        return intReversed
+    } else if (Math.sign(int) === -1) {
+        intReversed = parseInt(int.toString().split('-').join("").split("").reverse().join(""))
+        return -intReversed
+    }
   }
+
+  const handleReverse = (int) => {
+    const result = reverseInt(56)
+    setIntReversed(result)
+  } 
+    return (
+      <>
+      <div className="App">Hello World</div>
+      <form>
+        Integer: <input type="number" name="int"/>
+        <input type="submit" onClick={handleReverse}></input>
+      </form>
+      <h2>The Reverse is: {intReversed}</h2>
+      </>
+    )
 }
 
-export default App;
+export default App
